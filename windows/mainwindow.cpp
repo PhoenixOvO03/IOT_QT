ed3a6ae058654bcbb1b28ea41eab153a68751342
kangkang <2985385632@qq.com>
2024-04-24 22:16:18 +0800

    init

---
+++ windows/mainwindow.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    this->setWindowTitle(QString("IOT"));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
